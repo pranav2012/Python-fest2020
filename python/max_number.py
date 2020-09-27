9ffccafc65d693589b118f3a08f3ca611270552f
iriyagupta <riyagupta771995@gmail.com>
2020-09-27 21:05:02 +0530

    max_number

--- python/max_number.py
+++ python/max_number.py
@@ -3,12 +3,16 @@ import numpy as np
 
 input_num = [0.1, 0.3, 0.5, 0.7, 0.9, 0.5]
 
-#take the input from the user end and split it on the basis on ","
-#you can give the integer as input values too by using int instead of float
+#take the input from the user end and split it on the basis on "," you can give the integer as input values too by using int instead of float
 # input_num = list(map(float, input().split(",")))
+# input_num = list(map(int, input().split(",")))
 
+
+#defiene the function
 def max_number(input):
 	max_number = max(input)
-	print(max_number)
+	return max_number
 
-max_number(input_num)
+
+#call the max function
+print(max_number(input_num))
