4b8dbf30abb9ca44b8f5d6448871ad72b4892741
Bijoy-007 <67514758+Bijoy-007@users.noreply.github.com>
2020-09-27 21:16:41 +0530

    Adding 2 numbers
    Hello sir!
    It's my first open source  contribution.
    I have written the code please merge it

--- python/HelloWorld.py
+++ python/HelloWorld.py
@@ -1,1 +1,11 @@
-print("Hello World!!")
+#Considering that we're taking the input from user
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+
+
+#Now adding the numbers and storing it in a variable sum
+sum = float(num1) + float(num2)
+
+
+#result
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
