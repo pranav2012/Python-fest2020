5881ae990adb24f06192ac80051ca42ec1417dc5
Bijoy-007 <67514758+Bijoy-007@users.noreply.github.com>
2020-09-30 23:56:48 +0530

    Create SumOf2Numbers.cpp

---
+++ SumOf2Numbers.cpp
@@ -0,0 +1,9 @@
+# Taking 2 numbers as input from user
+num1 = input('Enter the first number: ')
+num2 = input('Enter the second number: ')
+
+# Adding two numbers and storing it in a variable
+sum = float(num1) + float(num2)
+
+# Displaying the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
