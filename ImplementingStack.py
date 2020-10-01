15430f9d1fc0caaff807229364d0d9be34cb05a8
Yash Mishra <53789050+Codedude1@users.noreply.github.com>
2020-10-01 12:31:59 +0530

    Create ImplementingStack.py

---
+++ ImplementingStack.py
@@ -0,0 +1,16 @@
+stack = [] 
+  
+stack.append('a') 
+stack.append('b') 
+stack.append('c') 
+  
+print('Initial stack') 
+print(stack) 
+  
+print('\nElements poped from stack:') 
+print(stack.pop()) 
+print(stack.pop()) 
+print(stack.pop()) 
+  
+print('\nStack after elements are poped:') 
+print(stack) 
