a7dd0485b0013f7aa5ae351b69e381188547ccfc
Rakshit Puri <32843491+skate1512@users.noreply.github.com>
2020-10-01 10:01:47 +0530

    Update timer.py

--- Timer/timer.py
+++ Timer/timer.py
@@ -1,8 +1,10 @@
 import time
-sec = int(input("Enter no. of seconds: "))
 
-for i in range(sec):
-    print(sec - i, "seconds left")
+# Taking time input from the user
+seconds = int(input("Enter no. of seconds: "))
+
+for i in range(seconds):
+    print(seconds - i, "seconds left")
     time.sleep(1)
 
 print("Time Over!!!!")
