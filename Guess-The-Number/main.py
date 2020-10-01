c8bbdd5882e84f6814ac137f5a082bdb50db1d7b
leo-20 <66257555+diyaRoy46@users.noreply.github.com>
2020-10-01 18:33:53 +0530

    Update main.py

--- Guess-The-Number/main.py
+++ Guess-The-Number/main.py
@@ -1,5 +1,3 @@
-#number guessing game
-# importing required modules
 import random
 
 # defining function named getchar 
@@ -21,17 +19,17 @@ def getchar():
 guess_number = 0
 
 # Print statements
-print("This is a number guessing game. \nYou will first choose a range of number. \nThen we will generate a secreat number.\nYou have to guess it in minimum number of tries.")
-print("If you enter your guess we will inform you either the number is smaller than the secreat number or bigger.")
+print("This is a number guessing game. \nYou will first choose a range of number. \nThen we will generate a secret number.\nYou have to guess it in minimum number of tries.")
+print("If you enter your guess we will inform you either the number is smaller than the secret number or bigger.")
 print("RULE : give all inputs as integers\nPress Enter" ,end = "")
 
 # Running while loop to find the range
 while getchar() and not guess_number:
     print("\nChoose Your Range")
     #print("Your number will be in between :")
-    print("Enter the starting number:", end = "")
+    print("Enter the starting number:")
     start = int(input())
-    print("Enter the ending number: ",end = "")
+    print("Enter the ending number: ")
     end = int(input())
     #guess_number = 0
     ans = random.randint(start,end)
@@ -39,12 +37,20 @@ while getchar() and not guess_number:
     	print("Your range is wrong")
     else:
       # Running a while loop inside a if statement
-        while True:
+      # When the user the guess_number reaches 5, the game will end
+        while (guess_number<5):
+           
             print("Enter your guess: ",end = "")
             user_input = int(input())
             guess_number+=1
+            
+            if guess_number==5:
+                print("Sorry no more guesses! You lost!")
+                break
+            
+            
             if user_input == ans:
-                print("Congratulations!! You have made it. \n This is the secreat number")
+                print("Congratulations!! You have made it. \n This is the secret number")
                 break
             elif user_input > ans:
                 print("Try something small")
