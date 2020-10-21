47cebe2ff2804c82b8b38a910b7ba2081bd50f05
GarimaDubey30 <73238370+GarimaDubey30@users.noreply.github.com>
2020-10-21 20:06:56 +0530

    Update factorial.py

--- python/factorial.py
+++ python/factorial.py
@@ -3,16 +3,10 @@ input_num = int(input("Enter the number:"))
 
 #define the factorial function
 def factorial(input_num):
-	f = 1
-	if input_num < 0:
-		print("Factorial cannot be calculated")
-	elif input_num == 0:
-		print("Factorial of 0 is 1")
+	if input_num==0 or input_num==1:
+		return 1
 	else:
-		for i in range(1,input_num+1):
-			f = f * i
-		return f
-
+		return input_num*factorial(input_num-1)
 
 #call the factorial function
 print(factorial(input_num))
